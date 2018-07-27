a6adc8ee45e9530f2d4e5c8a1f871ff5bab33fe5
Levi Senft <lsenft@gmail.com>
2018-07-27 13:52:53 -0600

    Get location via IP service.

--- app/app.js
+++ app/app.js
@@ -2,13 +2,36 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.calc',
-  'myApp.about',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ngRoute',
+    'myApp.calc',
+    'myApp.about',
+    'myApp.version'
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/calc'});
-}]);
+    $routeProvider.otherwise({redirectTo: '/calc'});
+}]).factory('ipLocation', ipLocation);
+
+ipLocation.$inject = ['$http'];
+
+function ipLocation($http) {
+    return {
+        getLocation: getLocation
+    };
+
+    function getLocation() {
+        const url = 'http://ip-api.com/json';
+
+        return $http.get(url)
+            .then(getLocationComplete)
+            .catch(getLocationFailed);
+
+        function getLocationComplete(response) {
+            return response.data;
+        }
+
+        function getLocationFailed(error) {
+            console.log('XHR Failed for ' + url + '.' + error.data);
+        }
+    }
+}
--- app/calc/calc.js
+++ app/calc/calc.js
@@ -2,13 +2,35 @@
 
 angular.module('myApp.calc', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/calc', {
-    templateUrl: 'calc/calc.html',
-    controller: 'CalcCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/calc', {
+            templateUrl: 'calc/calc.html',
+            controller: 'CalcCtrl'
+        });
+    }])
 
-.controller('CalcCtrl', [function() {
+    .controller('CalcCtrl', ['$scope', 'ipLocation', function ($scope, ipLocation, logger) {
+        $scope.dac = {
+            elevation: 0,
+            air_temperature: 0,
+            barometric_pressure: 0,
+            barometric_humidity: 0,
+            density_altitude: 'n/a',
+            relative_density: 'n/a'
+        };
 
-}]);
+        $scope.calculate = function() {
+            $scope.dac.density_altitude = $scope.dac.elevation + $scope.dac.air_temperature;
+        };
+
+        ipLocation.getLocation()
+            .then(function(data) {
+                $scope.location = {
+                    lat: data.lat,
+                    lng: data.lon,
+                    city: data.city,
+                    zip: data.zip
+                };
+                return data;
+            });
+    }]);
