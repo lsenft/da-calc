1d78cedaf03834c940633567b901bf75f68f4ca7
Levi Senft <lsenft@gmail.com>
2018-07-28 02:08:48 -0600

    typo

--- app/services/ipLocationService.js
+++ app/services/ipLocationService.js
@@ -3,7 +3,7 @@
 app.factory('ipLocationService', ['$http', function ($http) {
 
         const api_key = 'bc94d4c7a65731049143bda2b564f31b';
-        const url = 'http://api.ipstack.com/check?access_key=' + api_key;
+        const url = 'https://api.ipstack.com/check?access_key=' + api_key;
 
 
         return {
