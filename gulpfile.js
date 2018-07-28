44060cc306c3f7d30538bbcc92dab42c73858110
Levi Senft <lsenft@gmail.com>
2018-07-28 13:25:58 -0600

    Modify build to copy images.

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,16 @@ let gulp = require('gulp');
 let jshint = require('gulp-jshint');
 let uglify = require('gulp-uglify-es').default;
 let minifyCSS = require('gulp-minify-css');
-let clean = require('gulp-clean');
 let runSequence = require('run-sequence');
-let  useref = require('gulp-useref');
+let useref = require('gulp-useref');
 let gulpif = require('gulp-if');
+
+
+let onError = function(err) {
+    console.log(err);
+};
+
+
 // tasks
 gulp.task('lint', function() {
     gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
@@ -17,25 +23,16 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('fail'));
 });
 
-
-// gulp.task('minify-css', function() {
-//     var opts = {comments:true,spare:true};
-//     gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
-//         .pipe(minifyCSS(opts))
-//         .pipe(gulp.dest('./docs/'));
-// });
-// gulp.task('minify-js', function() {
-//     gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
-//         .pipe(uglify({
-//             // inSourceMap:
-//             // outSourceMap: "app.js.map"
-//         }))
-//         .pipe(gulp.dest('./docs/'));
-// });
 gulp.task('copy-bower-components', function () {
     gulp.src('./app/bower_components/**')
         .pipe(gulp.dest('docs/bower_components'));
 });
+
+gulp.task('copy-assets', function() {
+    gulp.src('./app/*.{png,json,xml,ico}')
+        .pipe(gulp.dest('./docs'));
+});
+
 gulp.task('html', function () {
     gulp.src('./app/**/*.html')
         .pipe(useref())
@@ -47,6 +44,6 @@ gulp.task('html', function () {
 
 gulp.task('build', function() {
     runSequence(
-        ['copy-bower-components', 'lint', 'html']
+        ['copy-bower-components', 'copy-assets', 'lint', 'html']
     );
 });
