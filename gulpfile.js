004bf2e798d6348b4128bdd2fbaff7e4036ca6dc
Levi Senft <lsenft@gmail.com>
2019-10-23 23:10:49 -0600

    update to gulp 4, remove run sequence.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ let gulp = require('gulp');
 let jshint = require('gulp-jshint');
 let uglify = require('gulp-uglify-es').default;
 let minifyCSS = require('gulp-minify-css');
-let runSequence = require('run-sequence');
 let useref = require('gulp-useref');
 let gulpif = require('gulp-if');
 var notify = require('gulp-notify');
@@ -15,35 +14,35 @@ var plumber = require('gulp-plumber');
 let onError = function(err) {
     notify.onError({
         title: "Gulp error in " + err.plugin,
-        message:  err.toString()
+        message: err.toString()
     })(err);
 };
 
 
 // tasks
 gulp.task('lint', function() {
-    gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
-        .pipe(plumber({ errorHandler: onError}))
+    return gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('copy-bower-components', function () {
-    gulp.src('./app/bower_components/**')
-        .pipe(plumber({ errorHandler: onError}))
+gulp.task('copy-bower-components', function() {
+    return gulp.src('./app/bower_components/**')
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(gulp.dest('docs/bower_components'));
 });
 
 gulp.task('copy-assets', function() {
-    gulp.src('./app/*.{png,json,xml,ico}')
-        .pipe(plumber({ errorHandler: onError}))
+    return gulp.src('./app/*.{png,json,xml,ico}')
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(gulp.dest('./docs'));
 });
 
-gulp.task('html', function () {
-    gulp.src('./app/**/*.html')
-        .pipe(plumber({ errorHandler: onError}))
+gulp.task('html', function() {
+    return gulp.src('./app/**/*.html')
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(useref())
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', minifyCSS()))
@@ -51,8 +50,8 @@ gulp.task('html', function () {
 });
 
 
-gulp.task('build', function() {
-    runSequence(
-        ['copy-bower-components', 'copy-assets', 'lint', 'html']
-    );
-});
+gulp.task('build',
+    gulp.series(
+        'copy-bower-components', 'copy-assets', 'lint', 'html'
+    )
+);
